abb26f4dfec9bce306492f2f5e39f7374a75b5be
Rodan Luzuriaga <rodanaluzuriaga@gmail.com>
2017-09-04 11:29:16 +0800

    jquery bootgrid and dragtable

--- assets/app/app-module.js
+++ assets/app/app-module.js
@@ -3,6 +3,6 @@ var app;
 (function() {
 
 	"use strict";
-	app = angular.module('skeletonApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
-
+	app = angular.module('skeletonApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'Dragtable']);
+	
 })();
