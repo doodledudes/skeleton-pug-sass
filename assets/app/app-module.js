88e89d65d233a04cc534e8e4c294cb1ee4d59c7d
doodledudes <rodanaluzuriaga@gmail.com>
2018-02-12 10:37:37 +0800

    new boostrap 4 settings update

--- assets/app/app-module.js
+++
@@ -1,8 +0,0 @@
-var app;
-
-(function() {
-
-	"use strict";
-	app = angular.module('skeletonApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'Dragtable']);
-	
-})();
--- assets/app/controllers/HomeController.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  "use strict";
-  app.controller('HomeController', function($scope, $rootScope) {
-
-  });
-})();
